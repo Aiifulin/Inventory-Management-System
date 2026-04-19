422c8304fac99c656cc433ad15900a1ae822c2b3
Justine Mendoza <mendozajustine2105@gmail.com>
2026-04-20 00:03:44 +0800

    feat: add firebase configuration file to manage app settings

---
+++ public/js/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey:            "AIzaSyBeaF2VKovHASuzhvZHzOoE0yB7QnBDej0",
+    authDomain:        "inventory-management-sys-baccc.firebaseapp.com",
+    projectId:         "inventory-management-sys-baccc",
+    storageBucket:     "inventory-management-sys-baccc.firebasestorage.app",
+    messagingSenderId: "304433839568",
+    appId:             "1:304433839568:web:50dafae1296e6bb0d30dd5",
+    measurementId:     "G-68CR9JCJV8"
+};
